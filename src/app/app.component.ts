83ab5f19337500adc3383248532e4264d22f96e3
imamhossainroni <imamhossainroni95@gmail.com>
2023-10-22 00:10:42 +0600

    angular event binding and event bubbling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-fundamentals';
+
+  blogs = [
+    'Blog post - 1',
+    'Blog post - 2',
+    'Blog post - 3',
+    'Blog post - 4',
+    'Blog post - 5',
+  ];
+  number = 1;
+
+  onClick(e: any) {
+    e.stopPropagation() // To avoid event bubbling
+    this.number += 1;
+  }
+
+  onClick1() {
+    console.log('Div-1 clicked')
+  }
+
+
+  onClick2() {
+    console.log('Div-2 clicked')
+  }
+
+
 }
